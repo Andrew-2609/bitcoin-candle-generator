1159377a6219dff903b97c6b94964c8b981e499e
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-11-16 23:17:11 -0300

    feat: add function to generate candles and call it at application startup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { config } from 'dotenv'
 import axios from 'axios'
+import Period from './enums/period'
+import { Candle } from './models/candle'
 
 config()
 
@@ -10,4 +12,24 @@ const readMarketPrice = async (): Promise<number> => {
   return price
 }
 
-readMarketPrice()
+const generateCandles = async () => {
+  while (true) {
+    const loopTimes = Period.ONE_MINUTE / Period.TEN_SECONDS
+    const candle = new Candle('BTC')
+
+    console.log('---\nGenerating new Candle\n---')
+    
+    for (let i = 0; i < loopTimes; i++) {
+      const price = await readMarketPrice()
+      candle.addValue(price)
+      console.log(`Market price #${i + 1} of ${loopTimes}`)
+      await new Promise((resolve) => setTimeout(resolve, Period.TEN_SECONDS))
+    }
+
+    candle.closeCandle()
+    console.log('Candle closed')
+    console.log(candle.toSimpleObject())
+  }
+}
+
+generateCandles()
